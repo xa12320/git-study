0d209987e98d97df54cfe377a7ee94b3a6706d47
XiaRong <1679000804@qq.com>
2023-02-06 21:31:39 +0800

    first commit

---
+++ src/main/java/com/study/GitTest.java
@@ -0,0 +1,12 @@
+package com.study;
+
+/**
+ * @Auther: Mr.Xia
+ * @Date: 2023/2/6 21:30
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
